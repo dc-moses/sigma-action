f0f3791d601dc5522628b6523bde6be424f98076
James Croall <jcroall@pobox.com>
2021-12-03 21:44:28 +0000

    Automated

--- dist/index.js
+++ dist/index.js
@@ -18523,7 +18523,8 @@ function run() {
             // Loop through findings and leave comments on lines
             for (var j = 0, len2 = obj["issues"]["issues"].length; j < len2; ++j) {
                 var issue = obj["issues"]["issues"][j];
-                if (issue['filepath'] in files_in_pr) {
+                //if (issue['filepath'] in files_in_pr) {
+                if (true) {
                     (0, core_1.info)(`Create PR comment on uuid=` + issue['uuid'] + " Checker: " + issue['checker_id'] + " Filepath: " + issue['filepath'] + " Line: " + issue['location']['start']['line']);
                 }
             }
