66401b0ada7076c6de61641c709a73df71c762c5
James Croall <jcroall@pobox.com>
2021-12-03 22:00:58 +0000

    Automated

--- dist/index.js
+++ dist/index.js
@@ -18522,13 +18522,16 @@ function run() {
                 var issue = obj["issues"]["issues"][j];
                 if (files_in_pr.includes(issue['filepath'])) {
                     (0, core_1.info)(`Create PR comment on uuid=` + issue['uuid'] + " Checker: " + issue['checker_id'] + " Filepath: " + issue['filepath'] + " Line: " + issue['location']['start']['line']);
+                    const sha = (0, github_context_1.getSha)();
                     var comment = yield octokit.rest.pulls.createReviewComment({
                         owner: contextOwner,
                         repo: contextRepo,
                         pull_number: contextIssue,
                         path: issue['filepath'],
                         body: "Sigma finding: " + issue['summary'],
-                        line: issue['location']['start']['line']
+                        line: issue['location']['start']['line'],
+                        commit_id: sha,
+                        position: 1
                     });
                     (0, core_1.info)(JSON.stringify(comment, null, 2));
                 }
